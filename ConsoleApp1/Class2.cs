866dba8c2179b649fecf483ba7a13db4feee388d
@shmuelandlers <66220749+shmelky613@users.noreply.github.com>
2020-10-22 01:33:32 -0400

    123

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClassLibrary1;
+
+
+
+namespace ConsoleApp1
+{
+   public class Class2 : Program
+    {
+
+       
+
+        public Class2() {
+
+
+          
+    }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using ClassLibrary1;
 
 
 namespace ConsoleApp1
 {
-    class Program 
+   public class Program 
     {
         private GameSession _gameSession;
 
@@ -24,16 +27,48 @@ namespace ConsoleApp1
             Console.WriteLine("Level: " + " " + _gameSession.CurrentPlayer.Level);
 
             Console.WriteLine("");
-          
 
-            void DoStuff()
+
+           void DoStuff()
             {
                 Console.WriteLine("enter your choice");
-                Console.WriteLine("1. hit");
+                Console.WriteLine("1. Hit   2. Exit");
+                Console.WriteLine("");
+
                 var bbb = Console.ReadLine();
 
 
-                if (bbb == "HIT" || bbb == "hit")
+                void e()
+                {
+                    if (bbb == "2")
+                    {
+                        Console.WriteLine("are you sure you want to exit ? '1: Yes   2: No' ");
+                        var ccc = Console.ReadLine();
+                        if (ccc == "1")
+                        {
+                            Console.WriteLine("exiting");
+                            for (var i = 0; i < 20000; i++)
+                            {
+                                Console.Write("😢");
+                            }
+                            Environment.Exit(0);
+                        }
+                        else if (ccc == "2")
+                        {
+                            DoStuff();
+                        }
+                        else
+                        {
+                            e();
+                        }
+                    }
+
+                }
+                e();
+
+
+                //hit
+                if (bbb == "1")
                 {
                     _gameSession.CurrentPlayer.HitPoints = _gameSession.CurrentPlayer.HitPoints + 10;
                     Console.WriteLine("");
@@ -41,16 +76,25 @@ namespace ConsoleApp1
                     Console.WriteLine("Name: " + " " + _gameSession.CurrentPlayer.Name);
                     Console.WriteLine("Character: " + " " + _gameSession.CurrentPlayer.CharacterClass);
                     Console.WriteLine("Hit Points: " + " " + _gameSession.CurrentPlayer.HitPoints);
+                    Console.WriteLine("Gold: " + " " + _gameSession.CurrentPlayer.Gold);
+                    Console.WriteLine("Experience Points: " + " " + _gameSession.CurrentPlayer.ExperiencePoints);
+                    Console.WriteLine("Level: " + " " + _gameSession.CurrentPlayer.Level);
+
                     Console.WriteLine(""); Console.WriteLine("");
                     DoStuff();
                 }
+
                 else
                 {
                     Console.WriteLine("invalid");
+                    DoStuff();
                 }
-            }
 
-            DoStuff();
+               
+             
+            }
+                
+                DoStuff();
         }
     }
 }
